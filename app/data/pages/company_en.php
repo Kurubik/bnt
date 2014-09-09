271f23096fb2f1c393b1568b97a03af2e023b144
jg <jg@vcgworld.com>
2014-09-09 16:45:21 +0300

    company page

--- app/data/pages/company_en.php
+++ app/data/pages/company_en.php
@@ -1,4 +1,23 @@
 <?php
  return array(
-
+    'title' => 'We are here 24/7 for your comfort',
+     'address' => '',
+     'phones' => array(
+         'usa' => array(
+             'name' => 'USA',
+             'number' => '+1 973 652-33-10',
+         ),
+         'russia' => array(
+             'name' => 'Russia',
+             'number' => '+7 964 771-38-00',
+         ),
+         'europe' => array(
+             'name' => 'Europe',
+             'number' => '+371 277-35-235',
+         ),
+         'ukraine' => array(
+             'name' => 'Ukraine',
+             'number' => '+380 44 392-21-75',
+         ),
+     ),
  );
