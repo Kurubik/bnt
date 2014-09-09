5599b3b4513c91131a360f4343d9bd4d7ee6eb56
inv <ka@vcgworld.com>
2014-09-09 16:43:38 +0300

    update plans & logo

--- app/data/data_en.php
+++ app/data/data_en.php
@@ -13,4 +13,13 @@ return array(
     ),
 
     'support' => 'Online support',
+
+    'tarif_minutes' => 'Minutes and internet (MB) included:',
+    'tarif_sms' => 'SMS included:',
+    'tarif_calls' => 'for incoming calls',
+    'time_connect' => 'One time connection fee',
+    'per_minute' => 'Overage — per minutes',
+    'out_sms' => 'Overage — outgoing SMS',
+    'tarif_internet' => 'Overage — internet'
+
 );
--- app/data/pages/plans_en.php
+++ app/data/pages/plans_en.php
@@ -2,46 +2,60 @@
 return array (
     'title' => 'Plans',
     'page_heading' => 'International mobile operator with flat rates.',
+    'download_text' => 'Download fact sheet',
 
     'plans' => array (
-        'individual' => array (
-            array (
-                'title' => 'Dialoq fuji',
-                'text' => 'Starters pack is perfectly suitable for active travelers and explorers.',
-                'list' => array (
-                    'units' => '2500 units for 375 €',
-                    'tarif' => 'Base tarif',
-                    'inet' => 'Minutes and internet (MB) included: <strong>2500</strong>',
-                    'sms' => 'SMS included: <strong>500</strong>',
-                    'number' => '<strong>1  local number</strong><br>for incoming calls',
-                    'conntect' => 'One time connection fee <strong>EUR 50</strong>',
-                    'per-min' => 'EUR 0.50 for first 250 mins, EUR 125 per 500 mins thereafter',
-                    'outgoing-sms' => 'EUR 0.10 for first 250 SMS, EUR 50 per 500 SMS thereafter',
-                    'overage-internet' => 'EUR 0.50 for first 250 MB, EUR 125 per 512 MB thereafter'
-                )
-            ),
+        array (
+            'title' => 'Dialoq fuji',
+            'text' => 'Starters pack is perfectly suitable for active travelers and explorers.',
+            'list' => array (
+                'units' => '2500 units for 375 €',
+                'tarif' => 'Base tarif',
+                'inet' => '2500',
+                'sms' => '500',
+                'number' => '1 local number',
+                  'conntect' => 'EUR 50',
+                  'per_min' => 'EUR 0.50 for first 250 mins, EUR 125 per 500 mins thereafter',
+                  'outgoing_sms' => 'EUR 0.10 for first 250 SMS, EUR 50 per 500 SMS thereafter',
+                  'overage_internet' => 'EUR 0.50 for first 250 MB, EUR 125 per 512 MB thereafter',
+                  'suspend' => 'Suspend a line N/A',
+                'button_text' => 'Order Fuji',
+
+            )
+        ),
           array (
             'title' => 'Dialoq mckinley',
             'text' => 'Excellent solution for frequent business trips and active internet users.',
             'list' => array (
-              'units' => '<strong>4000 units for 450 €</strong>',
+              'units' => '4000 units for 450 €',
               'tarif' => '30% economy',
-              'inet' => 'Minutes and internet (MB) included: <strong>4000</strong>',
-              'sms' => 'SMS included: <strong>750</strong>',
-              'number' => '<strong>2  local numbers</strong><br>for incoming calls'
+              'inet' => '4000',
+              'sms' => '750',
+              'number' => '2  local numbers',
+              'conntect' => 'EUR 50',
+              'per_min' => 'EUR 0.50 for first 250 mins, EUR 125 per 500 mins thereafter',
+              'outgoing_sms' => 'EUR 0.10 for first 250 SMS, EUR 50 per 500 SMS thereafter',
+              'overage_internet' => 'EUR 0.50 for first 250 MB, EUR 125 per 512 MB thereafter',
+              'suspend' => 'Suspend a line N/A',
+              'button_text' => 'Order McKinley',
             )
           ),
           array (
             'title' => 'Dialoq everest',
             'text' => 'Super-saver plan for the man of the world. Up to 50% of economy.',
             'list' => array (
-              'units' => '<strong>10000 units for 750 €</strong>',
+              'units' => '10000 units for 750 €',
               'tarif' => '30% economy',
-              'inet' => 'Minutes and internet (MB) included: <strong>10000</strong>',
-              'sms' => 'SMS included: <strong>1000</strong>',
-              'number' => '<strong>5  local numbers</strong><br>for incoming calls'
+              'inet' => '10000',
+              'sms' => '1000',
+              'number' => '5 local numbers',
+              'conntect' => 'EUR 50',
+              'per_min' => 'EUR 0.50 for first 250 mins, EUR 125 per 500 mins thereafter',
+              'outgoing_sms' => 'EUR 0.10 for first 250 SMS, EUR 50 per 500 SMS thereafter',
+              'overage_internet' => 'EUR 0.50 for first 250 MB, EUR 125 per 512 MB thereafter',
+              'suspend' => 'Suspend a line N/A',
+              'button_text' => 'Order Everest',
             )
           ),
         )
-    )
 );
