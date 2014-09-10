c55ee0dda4d2f870335dbe397155fcb0bf3b4b01
jg <jg@vcgworld.com>
2014-09-10 17:32:12 +0300

    company/contacts page fix

--- app/data/pages/company_en.php
+++ app/data/pages/company_en.php
@@ -1,26 +1,4 @@
 <?php
- return array(
-    'page_title' => 'We are here 24/7<br>for your comfort',
-     'title' => 'Company',
-     'contactInfo' => 'Dialoq CIA
-                       <br>ProiQuai de l’Ile 17 — CP 2251
-                       <br>1211 Genève 2em',
-     'phones' => array(
-         'usa' => array(
-             'name' => 'USA',
-             'number' => '<a href="tel:+19736523310">+1 973 652-33-10</a>',
-         ),
-         'russia' => array(
-             'name' => 'Russia',
-             'number' => '<a href="tel:+79647713800">+7 964 771-38-00</a>',
-         ),
-         'europe' => array(
-             'name' => 'Europe',
-             'number' => '<a href="tel:+37127735235">+371 277-35-235</a>',
-         ),
-         'ukraine' => array(
-             'name' => 'Ukraine',
-             'number' => '<a href="tel:+380443922175">+380 44 392-21-75</a>',
-         ),
-     ),
- );
+    return array(
+        'page_title' => 'Phasellus et euismod<br>elit 134. Vestib sceleri<br>ornare tellus.',
+    );
