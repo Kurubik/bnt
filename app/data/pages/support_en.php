689f646d33e045124d9d369580b676de1e571fb3
jg <jg@vcgworld.com>
2014-09-10 16:31:55 +0300

    push after merge

--- app/data/pages/support_en.php
+++ app/data/pages/support_en.php
@@ -3,5 +3,87 @@ return array (
     'title' => 'Support',
     'page_title' => 'In which countries can<br> subscribers get a local<br> phone number?',
     'question' => 'Question of the day',
+    'right_section' => array (
+        'title' => 'Support Centre',
+        'contacts' => 'USA<br><strong>+1 973 652-33-10</strong><br><br>Europe<br><strong>+371 277-35-235</strong><br><br>Russia',
+    ),
+
+    'support_list' => array (
+        array(
+            'title' => 'Coverage & Caller ID',
+            'list' => array (
+                array (
+                  'title' => 'How do I switch between operators?',
+                  'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                  'title' => 'I already have a mobile phone number in the United Sates, can I keep this number?',
+                  'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                  'title' => 'In which countries can subscribers get a local phone number?',
+                  'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                  'title' => 'What number is displayed on the caller ID with Dialoq sim card?',
+                  'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                    'title' => 'What number is displayed on the caller ID with Dialoq sim card?',
+                    'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+            )
+        ),
+        array (
+          'title' => 'plans & payment',
+          'list' => array (
+            array (
+              'title' => 'What is a subscription to a Dialoq plan?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+            array (
+              'title' => 'Is there any plans for business purposes?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+            array (
+              'title' => 'What’s “unit” stands for?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+            array (
+              'title' => 'How and when will a subscriber receive his/her bill?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+            array (
+              'title' => 'How can I start and pay for my subscription?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+            array (
+              'title' => 'Can I increase the amount of minutes, SMS, mobile Internet provided if I need more than the Max plan always for?',
+              'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+            ),
+          )
+        ),
+        array (
+            'title' => 'support and troubleshooting',
+            'list' => array (
+                array (
+                    'title' => 'What if the sim card is lost (lost, stolen, along with the device)?',
+                    'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                    'title' => 'What should I do if my sim card doesn’t work?',
+                    'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                  'title' => 'Does Dialoq support works 24/7 Worldwide? ',
+                  'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                ),
+                array (
+                    'title' => 'Is there any other ways to reach support service?',
+                    'text' => 'Subscriber will receive a bill via e-mail. At the request of the subscriber, the bill can be provided via standard mail. Usually it tooks about 1 day to get your bill via e-mail and 3–4 days for printed one on your billing address.'
+                )
+            )
+        )
+    ),
 
 );
