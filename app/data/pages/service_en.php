06427d403a3e66b01814f87026964abde64f96a4
inv <ka@vcgworld.com>
2014-09-11 10:36:48 +0300

    services array

--- app/data/pages/service_en.php
+++ app/data/pages/service_en.php
@@ -1,5 +1,40 @@
 <?php
 return array (
     'title' => 'Service',
-    'page_title' => 'Phasellus et euismod elit 134. Vestib sceleri ornare tellus.'
+    'page_title' => 'Phasellus et euismod elit 134. Vestib sceleri ornare tellus.',
+
+
+    'unit' => array (
+        'title' => 'What unit is?',
+        'heading' => '1 unit  — is a ultricies mauris. Sed at mauris consectetur, iaculis nisi vel, consequat.',
+        'text' => 'Dialoq is business mobile operator fringilla laoreet. Praesent vitae diam rhoncus, vulputate erat quis, malesuada lectus. Nam a ultricies mauris.',
+        'services' => array (
+            'first_title' => 'voice calls',
+            'second_title' => 'data service',
+            'third_title' => 'sms messages',
+            'first_text' => 'Duis ultricies ipsum eu turpis iaculis posuere satem acus.',
+            'second_text' => 'Pellentesque vitae lectus nec erat consectetur etam.',
+            'third_text' => 'Duis ultricies ipsum eu turpis iaculis posuere satem acus.'
+        ),
+        'more_text' => 'Curabitur gravida lectus vitae arcu sollicitudin sollicitudin. Duis ultricies ipsum eu turpis iaculis posuere.'
+    ),
+
+    'number' => array (
+        'title' => 'How local number works?',
+        'text' => 'Ut sodales risus gravida fringilla laoreet. Praesent vitae diam rhoncus, vulputate erat quis, malesuada lectus. Nam a ultricies mauris.',
+        'banner' => array (
+            'text' => 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices.  Nunc ut erat felis. Lorem ipsum et setra numa ut sodalers:',
+            'left_text' => 'People from UK (+44) or France (+39) call your local UK number. Principle works in <h4>opposite side.</h4>',
+            'first_number' => '+44',
+            'first_number_title' => 'Local number<br>United Kingdom',
+            'second_number' => '+39',
+            'second_number_title' => 'Local number France'
+        )
+    ),
+
+    'country' => array (
+        'title' => 'Countries & Coverage',
+        'heading' => 'Dialoq available in 235 countries all over the World.',
+
+    )
 );
