4419bcb3a0778d123be46aada35c45efbe0d7450
jg <jg@vcgworld.com>
2014-09-16 16:56:02 +0300

    popups

--- app/data/data_en.php
+++ app/data/data_en.php
@@ -75,4 +75,71 @@ return array(
     'emails' => array (
         'support@dialoq.com'
     ),
+
+    'popups' => array(
+        'terms' => array(
+            'heading' => 'Terms & Conditions',
+            'button' => 'Okay',
+            'tnc' => array(
+                '1' => 'Pient fuga. Et eation ra quo quam, as magnimus eum vento volor apicil id earum fugitat dit lat aut aut modios acerrum, nos sinvenimet es exero qui re nimi, sequas sum fuga. Ipsus, ipsaper ibusam fugiam faceatus molorro reperibus sed mi, velectur moluptur.',
+                '2' => 'Qui aut volupta por reperit ea comnihitiam aut que cus eritam quam faccabo rrorum resequam re quam aut etur mi, tem expla nus iunt parchictio et eliqui arumqui quae cuptas mo entiae esedign atatiunti ut pratet iusapitatem eria sum nonsequis coreprat entotaquid eum evelic tes everferibus quatur, sitibus et, que moluptatur res alit vent.',
+                '3' => 'Simus volorum quam eum conemped ut enisti offic temolup tatur, sequoditat rerita elitatecea des modissitatem nim et quidustia eumque veliqui beaque samustotam harum eiur.
+',
+                '4' => 'Tus esecata quasiti orrovitium id quiam unt.',
+                '5' => 'Piet volento taturec aborporrum sit dolupta voluptassi nimolo comnis abo. Nequi dolest am, omnis aut lant estrum que dolorem faceribus dolum fuga. Nequam repel il iliquam, sit assi doloris ipistrumet sedisciae sequosant.',
+            ),
+        ),
+        'sent' => array(
+            'heading' => 'Thank you',
+            'text' => 'Your order has been successfully placed.
+Please check your e-mail for more details.',
+        ),
+        'order_sim' => array(
+            'heading' => 'Order Dialoq sim-card',
+            'button' => 'Order',
+            'order_type' => array(
+                'individual' => 'Individual',
+                'legal_entity' => 'Legal entity',
+            ),
+            'form' => array(
+                'headings' => array(
+                    'pers_info' => 'Personal info',
+                    'plan' => 'Plan',
+                    'sim' => 'Sim-card',
+                    'agreement' => 'I have read and agree to the terms and conditions',
+                ),
+                'info_fields' => array(
+                    'titles' => array(
+                        'name' => 'Name',
+                        'phone' => 'Phone',
+                        'email' => 'E-mail',
+                    ),
+                    'errors' => array(
+                        'name' => 'Please enter your name',
+                        'phone' => 'Please enter your phone',
+                        'email' => ' Please enter your e-mail',
+                    ),
+                ),
+                'plans' => array(
+                    '1' => array(
+                        'title' => 'Dialoq 3K',
+                        'conditions' => '2500 units for 375 €',
+                    ),
+                    '2' => array(
+                        'title' => 'Dialoq 5K',
+                        'conditions' => '4000 units for 450 €',
+                    ),
+                    '3' => array(
+                        'title' => 'Dialoq 10K',
+                        'conditions' => '10000 units for 750 €',
+                    ),
+                ),
+                'sim_types' => array(
+                    'nano' => 'Nano',
+                    'micro' => 'Micro',
+                ),
+            ),
+        ),
+
+    ),
 );
