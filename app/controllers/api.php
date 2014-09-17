865a34462ab97cef621a62a4037284455f4d9184
inv <ka@vcgworld.com>
2014-09-17 12:58:11 +0300

    added currency field

--- app/controllers/api.php
+++ app/controllers/api.php
@@ -15,17 +15,18 @@ $api->post('/payment/', function() use ($app) {
     $generator = new lib\Signature\SignatureGenerator('A4KedhE55XeqWHXN3+exDKES4p8=');
     $site_id = 245;
     $external_id = rand();
+    $currency = 'USD';
     $amount = (int)$_POST['plan'];
 
     $requestParams = array (
-        'site_id' => $site_id,
         'amount' => $amount,
+        'currency' => $currency,
+        'site_id' => $site_id,
         'external_id' => $external_id,
     );
 
     $signature = $requestParams['signature'] = $generator->assemble($requestParams);
-//    $src = "https://terminal-sandbox.ecommpay.com/?site_id=245&amount=100&external_id=1&signature={{ signature }}";
-    $src = 'https://terminal-sandbox.ecommpay.com/?site_id=' . $site_id . '&amount=' . $amount . '&external_id=' . $external_id . '&signature=' . $signature;
+    $src = 'https://terminal-sandbox.ecommpay.com/?site_id=' . $site_id . '&amount=' . $amount . '&currency=' . $currency . '&external_id=' . $external_id . '&signature=' . $signature;
     return $src;
 
 })->assert('_locale', 'en');
