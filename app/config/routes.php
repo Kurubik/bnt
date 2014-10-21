47adcfcff2138052bd4dde07792bf6dbabf86148
Aleksey Kupich <ljoha1@inbox.lv>
2014-10-21 12:20:31 +0000

    routes.php edited online with Bitbucket

--- app/config/routes.php
+++ app/config/routes.php
@@ -6,8 +6,6 @@
 
 require_once ROOT . '/app/data/control/data.php';
 require_once ROOT . '/app/data/control/validate.php';
-require_once ROOT . '/app/lib/SignatureGenerator.php';
-require_once ROOT . '/app/model/Order.php';
 
 $app->mount('/', require ROOT . '/app/controllers/pages.php');
 $app->mount('/{_locale}/api/', require ROOT . '/app/controllers/api.php');
