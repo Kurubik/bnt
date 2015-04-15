1800e6e32c6f3a8b17fea405cba6459e01e3787d
JG <abovezenith@gmail.com>
2015-04-15 20:01:58 +0300

    career page added

---
+++ app/data/pages/career_en.php
@@ -0,0 +1,45 @@
+<?php
+return array(
+    'heading' => array(
+        'backgroud' => '/public/assets/images/projects/ecofuel.jpg',
+        'title' => 'Career',
+        'text' => 'Agnit que moluptatur. Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Rionse et excero di ut qui bla inusandic tem.Obis escimod ut.',
+        'sidetext' => 'Ximos es eostium essus moloreptas id ut facipicta vit, tem excestias aborectus molliame voluptatem quis',
+    ),
+    'apply' => array(
+        'apply_title' => 'Apply now:',
+        'apply_mail' => 'carreer@bntech.lv',
+        'vacancies' => array(
+            '1' => array(
+                'heading' => 'Leading Engineer',
+                'text' => 'Agnit que moluptatur. Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Dolupta sitia quae: Icaborib usapis aut posamet maios si as evel intoreruptas si adis et.',
+                'points' => array(
+                    'Set up of Baltic New Technologies, project SPV domiciled in Latvia',
+                    'Long-term land lease agreement signed with Free Port of Ventspils',
+                    'Investment Agreement signed with Free Port of Ventspils',
+                    'Goverment of the Republic of Latvia approved tax relief in the amount of EUR 92M (subject to EC aproval)',
+                    'Feasibility studies completed by KBR ',
+                    'Negotiations with Licensor and Engineering Contractors in progress',
+                    'Memorandum of Understanding signed with Vitol Group,  leading public energy and commodity company',
+                    'Memorandum of Understanding signed with VNT',
+                    'Core project team comprising top-ranked industry professionals',
+                ),
+            ),
+            '2' => array(
+                'heading' => 'Technology Controller',
+                'text' => 'Agnit que moluptatur. Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Dolupta sitia quae: Icaborib usapis aut posamet maios si as evel intoreruptas si adis et.',
+                'points' => array(
+                    'Set up of Baltic New Technologies, project SPV domiciled in Latvia',
+                    'Long-term land lease agreement signed with Free Port of Ventspils',
+                    'Investment Agreement signed with Free Port of Ventspils',
+                    'Goverment of the Republic of Latvia approved tax relief in the amount of EUR 92M (subject to EC aproval)',
+                    'Feasibility studies completed by KBR ',
+                    'Negotiations with Licensor and Engineering Contractors in progress',
+                    'Memorandum of Understanding signed with Vitol Group,  leading public energy and commodity company',
+                    'Memorandum of Understanding signed with VNT',
+                    'Core project team comprising top-ranked industry professionals',
+                ),
+            ),
+        ),
+    ),
+);
