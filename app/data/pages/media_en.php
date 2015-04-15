d79f3c5c43d7f263340c70f41d89bc47d1bfa32e
JG <abovezenith@gmail.com>
2015-04-15 13:20:41 +0300

    media page

--- app/data/pages/media_en.php
+++ app/data/pages/media_en.php
@@ -11,4 +11,47 @@ return array(
         'link_text' => 'download press kit <span>3.5 MB</span>',
         'link' => '',
     ),
+    'news' => array(
+        'news_pagination' => array(
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '&hellip;',
+            '12'
+        ),
+        'content' => array(
+            '1' => array(
+                'date' => 'Apr 10, 2015',
+                'title' => 'Quisque lobortis nisl ac bibendum nouevo',
+                'text' => 'Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Sequam, eum resent es rerioris doloribus: Ullabore heniend ipient aut volore nimin eumquassunt ea nobiscipsam as iduciis eaturibea eosti bersped quas ipicabo.',
+                'link' => 'news',
+            ),
+            '2' => array(
+                'date' => 'Mar 30, 2015',
+                'title' => 'Quisque lobortis nisl ac bibendum nouevo',
+                'text' => 'Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Sequam, eum resent es rerioris doloribus: Ullabore heniend ipient aut volore nimin eumquassunt ea nobiscipsam as iduciis eaturibea eosti bersped quas ipicabo.',
+                'link' => 'news',
+            ),
+            '3' => array(
+                'date' => 'Feb 14, 2015',
+                'title' => 'Quisque lobortis nisl ac bibendum nouevo',
+                'text' => 'Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Sequam, eum resent es rerioris doloribus: Ullabore heniend ipient aut volore nimin eumquassunt ea nobiscipsam as iduciis eaturibea eosti bersped quas ipicabo.',
+                'link' => 'news',
+            ),
+            '4' => array(
+                'date' => 'Apr 10, 2015',
+                'title' => 'Quisque lobortis nisl ac bibendum nouevo',
+                'text' => 'Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Sequam, eum resent es rerioris doloribus: Ullabore heniend ipient aut volore nimin eumquassunt ea nobiscipsam as iduciis eaturibea eosti bersped quas ipicabo.',
+                'link' => 'news',
+            ),
+            '5' => array(
+                'date' => 'Mar 30, 2015',
+                'title' => 'Quisque lobortis nisl ac bibendum nouevo',
+                'text' => 'Quisque non ne re plibus audis receatur abo. Nam, sum voluptaque. Ulpari quias essim fuga. Sequam, eum resent es rerioris doloribus: Ullabore heniend ipient aut volore nimin eumquassunt ea nobiscipsam as iduciis eaturibea eosti bersped quas ipicabo.',
+                'link' => 'news',
+            ),
+        ),
+    ),
 );
